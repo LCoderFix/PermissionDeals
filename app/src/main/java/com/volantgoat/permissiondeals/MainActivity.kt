31988ab6fcd5dd91f8a5f65afa8e3c2b8cfec9d3
刘军东 <507085831@qq.com>
2020-11-19 09:58:50 +0800

    Initial commit

---
+++ app/src/main/java/com/volantgoat/permissiondeals/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.volantgoat.permissiondeals
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.volantgoat.library.PermissionDeals
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        makeCallBtn.setOnClickListener {
+            PermissionDeals.request(this, android.Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:10086")
+        startActivity(intent)
+    }
+}
